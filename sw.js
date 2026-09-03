05974a1adf5d21b86d210199755d46f8db784a42
aimanusman0331-star <aimanusman0331@gmail.com>
2026-09-04 02:34:51 +0500

    Add files via upload
    Upgrade ADAPT-X to V8 3D Battle Royale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='adaptx-v7-br-1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const C='adaptx-v8-3d-1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
