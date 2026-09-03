28b02ecdc17c084d3a5279901d02f962b8f99a2a
aimanusman0331-star <aimanusman0331@gmail.com>
2026-09-04 02:49:47 +0500

    Add files via upload
    Fix V8 3D camera and spawn visibility

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='adaptx-v8-3d-1';
+const C='adaptx-v8-1-3d-fix';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
